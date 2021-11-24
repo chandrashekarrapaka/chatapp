7dbee884358bac7f1a5b98fda5a1206e842c4505
rapaka.chandrashekar@gmail.com <rapaka.chandrashekar@gmsil.com>
2021-11-24 15:21:15 +0530

    chatbotset

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import {ChatEngine} from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App=()=>{
+    return(
+        <ChatEngine height="100vh" 
+        projectID="d4add656-7b41-463a-b114-8e5f9cefedda"
+         userName="Chandu" 
+         userSecret="secret"
+         renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps}/>}
+        />
+
+
+    );
+};
 
 export default App;
